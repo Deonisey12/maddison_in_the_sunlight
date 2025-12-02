b43af4365200678b3fba8c7ee7a8b1a57596f80a
D.Pereverzev <07031865y@gmail.com>
2025-12-03 02:01:03 +0300

    Fix items tags

--- src/generators/item.py
+++ src/generators/item.py
@@ -6,8 +6,8 @@ from entity import Entity
 class Item(Entity):
     additional_prm = ["isUsable"]
 
-    def __init__(self, name, disc="empty", isUsable=False) -> None:
-        super().__init__(name, disc)
+    def __init__(self, name, disc="empty", tags=[], isUsable=False) -> None:
+        super().__init__(name, disc, tags)
         self._path = "local/items"
         self._isUsable = isUsable
         
