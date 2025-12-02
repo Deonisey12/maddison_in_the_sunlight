00e6d6598dbbfd87fa735edd556ad3e63de4e481
D.Pereverzev <07031865y@gmail.com>
2025-12-03 00:41:09 +0300

    Add bot backend

---
+++ src/bot/bot.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append("src/bot")
+
+import logging
+
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+from commands import BotCommands
+
+
+class TelegramBot():
+
+    def __init__(self, token_path=""):
+        with open(token_path, "r") as f:
+            self.__token = f.read()
+            self.__cmd = BotCommands
+
+        logging.basicConfig(
+            format="%(asctime)s-%(name)s-%(levelname)s-%(message)s", level=logging.INFO
+        )
+        logging.getLogger("httpx").setLevel(logging.WARNING)
+        self.logger = logging.getLogger(__name__)
+
+    def Start(self):
+        application = Application.builder().token(self.__token).build()
+
+        application.add_handler(CommandHandler("start", self.__cmd.start))
+        application.add_handler(CommandHandler("help", self.__cmd.help_command))
+
+        application.run_polling(allowed_updates=Update.ALL_TYPES)
---
+++ src/bot/commands.py
@@ -0,0 +1,21 @@
+from generators.generator import Generator
+
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+
+class BotCommands():
+
+    async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        user = update.effective_user
+        await update.message.reply_html(
+            rf"Hi {user.mention_html()}!",
+            # reply_markup=ForceReply(selective=True),
+        )
+
+    async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        await update.message.reply_text("Help!")
+
+    async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        await update.message.reply_text(update.message.text)
+        
