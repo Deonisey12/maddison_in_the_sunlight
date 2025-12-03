230b6845ed40c6ce9efb3ae900731d49b7717a55
D.Pereverzev <07031865y@gmail.com>
2025-12-03 14:47:07 +0300

    Refactoring bot

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -3,8 +3,11 @@ sys.path.append("src/bot")
 
 import logging
 
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import *
+from telegram.ext import *
+
+from functools import partial
+
 
 from commands import BotCommands
 
@@ -22,12 +25,18 @@ class TelegramBot():
         logging.getLogger("httpx").setLevel(logging.WARNING)
         self.logger = logging.getLogger(__name__)
 
+        self._app = Application.builder().token(self.__token).build()
+
+    def addCmdHandlers(self):
+        self._app.add_handler(CommandHandler("start", partial(self.__cmd.start, self.__cmd)))
+        self._app.add_handler(CommandHandler("test", partial(self.__cmd.echo, self.__cmd)))
+        self._app.add_handler(CommandHandler("create", partial(self.__cmd.create, self.__cmd)))
+        self._app.add_handler(CommandHandler("form", partial(self.__cmd.test_form, self.__cmd)))
+
+    def addCallbackHandlers(self):
+        self._app.add_handler(CallbackQueryHandler(partial(self.__cmd.button_callback, self.__cmd)))
+
     def Start(self):
-        application = Application.builder().token(self.__token).build()
-
-        application.add_handler(CommandHandler("start", self.__cmd.start))
-        application.add_handler(CommandHandler("test", self.__cmd.echo))
-
-        application.add_handler(CommandHandler("create", self.__cmd.create))
-
-        application.run_polling(allowed_updates=Update.ALL_TYPES)
+        self.addCmdHandlers()
+        self.addCallbackHandlers()
+        self._app.run_polling(allowed_updates=Update.ALL_TYPES)
