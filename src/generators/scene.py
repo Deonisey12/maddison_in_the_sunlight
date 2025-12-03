4769f07b5b741f9a3bd40039cd445fc246ee356d
D.Pereverzev <07031865y@gmail.com>
2025-12-03 11:57:27 +0300

    Add to scene img path holder

--- src/generators/scene.py
+++ src/generators/scene.py
@@ -5,10 +5,12 @@ from entity import Entity
 
 class Scene(Entity):
     additional_prm = [
-        "isGlobal"
+        "isGlobal",
+        "img_path"
     ]
 
-    def __init__(self, name, disc="empty", tags="", isGlobal=True) -> None:
+    def __init__(self, name, disc="empty", tags="", isGlobal=True, img=None) -> None:
         super().__init__(name, disc, tags)
         self._path = "local/scenes"
         self._isGlobal = isGlobal
+        self._img_path = img
