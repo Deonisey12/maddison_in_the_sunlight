3d606521e13dd81941aa4e5f3525572c4acf9497
D.Pereverzev <07031865y@gmail.com>
2025-12-05 14:49:07 +0300

    Start cmd: now remove user data

--- src/bot/commands/start_command.py
+++ src/bot/commands/start_command.py
@@ -10,7 +10,9 @@ from .base_command import BaseCommand
 class StartCommand(BaseCommand):
     async def execute(self, update: tg.Update, context: tgx.ContextTypes.DEFAULT_TYPE):
         user = update.effective_user
+        context.user_data.clear()
+        
         await update.message.reply_html(
-            rf"Hi {user.name}!"
+            rf"Ыыыыы"
         )
 
