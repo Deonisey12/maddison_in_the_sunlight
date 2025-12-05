f9887723fe899eafe60c479df8a2d809c3a2399b
D.Pereverzev <07031865y@gmail.com>
2025-12-05 19:05:22 +0300

    Fix: character create

--- src/entities/character.py
+++ src/entities/character.py
@@ -5,54 +5,54 @@ from entity import Entity
 
 class Character(Entity):
     additional_prm = [
-        "attributes_physical",
-        "attributes_social",
-        "attributes_mental",
+        "attributesPhysical",
+        "attributesSocial",
+        "attributesMental",
 
-        "abilities_talents",
-        "abilities_skills",
-        "abilities_knowledges",
+        "abilitiesTalents",
+        "abilitiesSkills",
+        "abilitiesKnowledges",
 
         "willpower"
     ]
 
     Path = "local/characters"
 
-    def __init__(self, id: int = 0, name="EMPTY_NAME", disc="EMPTY_DISCRIPTION", tags=[], 
-                 attributes_physical=0, attributes_social=0, attributes_mental=0,
-                 abilities_talents=0, abilities_skills=0, abilities_knowledges=0, willpower=0) -> None:
+    def __init__(self, id: int = 0, name="EMPTYNAME", disc="EMPTYDISCRIPTION", tags=[], 
+                 attributesPhysical=0, attributesSocial=0, attributesMental=0,
+                 abilitiesTalents=0, abilitiesSkills=0, abilitiesKnowledges=0, willpower=0) -> None:
         super().__init__(id, name, disc, tags)
-        self._attributes_physical = int(attributes_physical)
-        self._attributes_social = int(attributes_social)
-        self._attributes_mental = int(attributes_mental)
-        self._abilities_talents = int(abilities_talents)
-        self._abilities_skills = int(abilities_skills)
-        self._abilities_knowledges = int(abilities_knowledges)
+        self._attributesPhysical = int(attributesPhysical)
+        self._attributesSocial = int(attributesSocial)
+        self._attributesMental = int(attributesMental)
+        self._abilitiesTalents = int(abilitiesTalents)
+        self._abilitiesSkills = int(abilitiesSkills)
+        self._abilitiesKnowledges = int(abilitiesKnowledges)
         self._willpower = int(willpower)
 
     @property
-    def attributes_physical(self):
-        return self._attributes_physical
+    def attributesPhysical(self):
+        return self._attributesPhysical
 
     @property
-    def attributes_social(self):
-        return self._attributes_social
+    def attributesSocial(self):
+        return self._attributesSocial
 
     @property
-    def attributes_mental(self):
-        return self._attributes_mental
+    def attributesMental(self):
+        return self._attributesMental
 
     @property
-    def abilities_talents(self):
-        return self._abilities_talents
+    def abilitiesTalents(self):
+        return self._abilitiesTalents
 
     @property
-    def abilities_skills(self):
-        return self._abilities_skills
+    def abilitiesSkills(self):
+        return self._abilitiesSkills
 
     @property
-    def abilities_knowledges(self):
-        return self._abilities_knowledges
+    def abilitiesKnowledges(self):
+        return self._abilitiesKnowledges
 
     @property
     def willpower(self):
