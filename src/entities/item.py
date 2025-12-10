bac19790792c0563ada649a6166892d4338213e7
D.Pereverzev <07031865y@gmail.com>
2025-12-10 16:09:01 +0300

    items: fix naming

--- src/entities/item.py
+++ src/entities/item.py
@@ -7,16 +7,16 @@ class Item(Entity):
     additional_prm = [
         "isUsable",
         "function",
-        "function_params"
+        "functionParams"
         ]
 
     Path = "local/items"
 
-    def __init__(self, id: int=0, name="EMPTY NAME", disc="EMPTY DISCRIPTION", tags=[], isUsable=False, function=None, function_params=[]) -> None:
+    def __init__(self, id: int=0, name="EMPTY NAME", disc="EMPTY DISCRIPTION", tags=[], isUsable=False, function=None, functionParams=[]) -> None:
         super().__init__(id, name, disc, tags)
         self._isUsable = isUsable
         self._function = function
-        self._function_params = function_params
+        self._functionParams = functionParams
 
     @property
     def isUsable(self):
