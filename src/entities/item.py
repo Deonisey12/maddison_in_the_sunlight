3cc215106855f4f75d6e085a8dce6a30aee3bb89
D.Pereverzev <07031865y@gmail.com>
2025-12-10 14:00:22 +0300

    items: add new params

--- src/entities/item.py
+++ src/entities/item.py
@@ -4,15 +4,24 @@ sys.path.append("src/entities")
 from entity import Entity
 
 class Item(Entity):
-    additional_prm = ["isUsable"]
+    additional_prm = [
+        "isUsable",
+        "function",
+        "function_params"
+        ]
 
     Path = "local/items"
 
-    def __init__(self, id: int=0, name="EMPTY NAME", disc="EMPTY DISCRIPTION", tags=[], isUsable=False) -> None:
+    def __init__(self, id: int=0, name="EMPTY NAME", disc="EMPTY DISCRIPTION", tags=[], isUsable=False, function=None, function_params=[]) -> None:
         super().__init__(id, name, disc, tags)
         self._isUsable = isUsable
+        self._function = function
+        self._function_params = function_params
 
     @property
     def isUsable(self):
         return self._isUsable
         
+    @property
+    def function(self):
+        return self._function
