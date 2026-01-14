44c66f12ace2a2fc703fa0485d7579c4ad69365d
D.Pereverzev <07031865y@gmail.com>
2026-01-14 20:53:28 +0300

    fix: scene prms on init

--- src/entities/scene.py
+++ src/entities/scene.py
@@ -5,21 +5,41 @@ from entity import Entity
 
 class Scene(Entity):
     additional_prm = [
+        "events",
         "isGlobal",
-        "img_path"
+        "imgPath"
     ]
 
     Path = "local/scenes"
 
-    def __init__(self,id: int=0, name="EMPTY NAME", disc="EMPTY DISCRIPTION", tags=[], isGlobal=True, img=None) -> None:
+    def __init__(self,id: int=0, name="EMPTY NAME", disc="EMPTY DISCRIPTION", tags=[], events = "", isGlobal=True, img=None) -> None:
         super().__init__(id, name, disc, tags)
-        self._isGlobal = isGlobal
-        self._img_path = img
+
+        if type(isGlobal == str):
+            if isGlobal == 'True':
+                self._isGlobal = True
+            else:
+                self._isGlobal = False
+        else:
+            self._isGlobal = isGlobal
+        
+        self._imgPath = img
+        if img == "None":
+            self._imgPath = None
+        self._events = []
+        if events != "":
+            events = events[1:-1]
+            for e in events.split(', '):
+                self._events.append(int(e))
+
+    @property
+    def events(self):
+        return self._events
 
     @property
     def isGlobal(self):
         return self._isGlobal
 
     @property
-    def img_path(self):
-        return self._img_path
+    def imgPath(self):
+        return self._imgPath
