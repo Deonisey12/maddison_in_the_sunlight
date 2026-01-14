bffdf309ef3985f4b45584573ef6505549678a1f
D.Pereverzev <07031865y@gmail.com>
2026-01-14 18:36:19 +0300

    fix: get function return

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -1,17 +1,12 @@
-import sys
-sys.path.append("src/functions")
-
-from functions import (
-    AddFunction,
-    RemoveFunction,
-    ChangeFunction,
-    AddManyFunction,
-    RemoveManyFunction,
-    ChangeManyFunction,
-    ChallengeFunction,
-    PayChallengeFunction,
-    RelaxFunction,
-)
+from .add_func import AddFunction
+from .remove_func import RemoveFunction
+from .change_func import ChangeFunction
+from .add_many_func import AddManyFunction
+from .remove_many_func import RemoveManyFunction
+from .change_many_func import ChangeManyFunction
+from .challenge_func import ChallengeFunction
+from .pay_challenge_func import PayChallengeFunction
+from .relax_func import RelaxFunction
 
 class Functions:
     Functions = {
@@ -29,5 +24,5 @@ class Functions:
     def GetFunction(function_name: str):
         function_class = Functions.Functions.get(function_name, None)
         if function_class:
-            return function_class.execute
+            return function_class()
         return None
