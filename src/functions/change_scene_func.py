10352f8b873ec62df4bf8429b9cbe9a037f1a5bb
D.Pereverzev <07031865y@gmail.com>
2026-01-30 18:47:13 +0300

    add function for change scene

---
+++ src/functions/change_scene_func.py
@@ -0,0 +1,11 @@
+from src.entities import Location
+from src.users import UserData
+from src.functions import Function
+
+class ChangeSceneFunction(Function):
+    def execute(self, user_data: UserData, scene_id: int, cost_item_id: int = -1, cost_count: int = 1):
+        if cost_item_id != -1:
+            if user_data.RmItemFromInventory(cost_item_id, cost_count):
+                user_data.Scene = scene_id
+        else:
+            user_data.Scene = scene_id
